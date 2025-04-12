59f040894ded9324e79596d6647d430d454e2c30
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-04-12 10:24:45 +0000

    math and num

---
+++ trial/numbers.js
@@ -0,0 +1,22 @@
+const num = 1;
+console.log(num);
+
+const no = new Number(123.9999999999);
+
+console.log(no);
+console.log(no.toString());
+console.log(no.toFixed(5));
+console.log(no.toPrecision(2));
+
+const no1 = 100000
+console.log(no1.toLocaleString());
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+console.log(Math.sqrt(64));
+console.log(Math.ceil(5.6));
+console.log(Math.floor(5.6));
+console.log(Math.min(5,7,8,9));
+console.log(Math.max(5,7,8,9));
+
+console.log(Math.ceil(Math.random()*10));
