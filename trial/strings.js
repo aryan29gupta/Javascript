d4aeb1a0b90b1c2f44c614196ef866caad10a3f1
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-04-12 10:02:40 +0000

    STRINGS

---
+++ trial/strings.js
@@ -0,0 +1,23 @@
+const a = new String("ARYAN GUPTA");
+
+console.log(a.length);
+console.log(a[1]);
+
+console.log(a.charAt(2));
+console.log(a.indexOf('Y'));
+
+console.log(a.substring(0,5));
+console.log(a.slice(-11,5));
+
+let newstr = "    Hello    ";
+
+console.log(newstr);
+console.log(newstr.trim());
+
+console.log(a.includes('ry'));
+
+
+console.log(newstr.replace('H','h'));
+
+console.log(a.split(" "));
+
