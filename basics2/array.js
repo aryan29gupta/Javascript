76f936d847a48861e72b455645370ca0cdaa1eb7
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-04-13 09:41:27 +0000

    arrays

---
+++ basics2/array.js
@@ -0,0 +1,28 @@
+const arr = [0,1,2,3,4,5];
+
+/*console.log(arr);
+
+arr.push(6);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(8);
+console.log(arr);
+
+arr.shift()
+console.log(arr);
+*/
+
+new_arr = arr.join();
+console.log(new_arr);
+
+a= arr.slice(1,3);
+b=arr.splice(1,3);
+
+console.log(arr);
+console.log(a);
+console.log(b);
+
+
---
+++ basics2/array2.js
@@ -0,0 +1,31 @@
+const fruits = ["apple","kiwi","cheeku"];
+const vegetables = ["ladyfinger","potato","tomato"];
+/*
+fruits.push(vegetables);
+console.log(fruits);
+
+const mix2 = fruits.concat(vegetables);
+console.log(mix2);
+
+const mix3 = [...fruits,...vegetables];
+console.log(mix3);
+*/
+
+//const str = "Aryan";
+//console.log(Array.from(str));
+
+
+//const Arr = str.split("");
+//console.log(Arr);
+
+let a = 100;
+let b = 200;
+let c = 300;
+
+let Arr = [a,b,c];
+console.log(Arr);
+
+let newArr = (Array.of(a,b,c));
+console.log(newArr);
+
+
