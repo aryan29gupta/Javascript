c2f6e1e595f4ac83521d4433d92daa0fca6c04d8
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-07 08:21:54 +0000

    JSON

---
+++ basics2/objects3.js
@@ -0,0 +1,25 @@
+const area = {
+    country:"India",
+    state:"Delhi",
+    city:"New Delhi",
+}
+
+const {country: cont} = area
+console.log(cont)
+
+
+
+
+//JSON FORMAT
+
+/*{
+    "name" : "Aryan",
+    "age" : "18",
+}
+
+[
+    {},
+    {},
+    {}
+]
+    */
