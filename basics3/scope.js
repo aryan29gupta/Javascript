580c921485f131482310dccbbbf586c6466d9188
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-10 08:53:28 +0000

    Scopes

---
+++ basics3/scope.js
@@ -0,0 +1,34 @@
+// let a = 20;
+// const b = 30;
+// var c = 40;
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// if (true){
+//     // let a = 20
+//     // const b = 30
+//     var c = 40      // var will always work inside 
+// }
+
+// // console.log(a)
+// // console.log(b)
+// console.log(c)
+
+
+//Local variables cannot be accessed outside the function
+
+let b = 100
+if (true){
+   
+    const b = 30
+    console.log(b);
+    
+}
+
+// console.log(a)
+console.log(b)
+// console.log(c)
+
+//global scope is different in browser and when we run it in node
