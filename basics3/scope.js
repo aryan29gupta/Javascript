ed4d6b690d16675c556da2a27427bc5a4c03a0dc
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-11 17:58:39 +0000

    arrow

--- basics3/scope.js
+++ basics3/scope.js
@@ -1,37 +1,37 @@
-// // let a = 20;
-// // const b = 30;
-// // var c = 40;
+let a = 20;
+const b = 30;
+var c = 40;
 
-// // console.log(a)
-// // console.log(b)
-// // console.log(c)
+console.log(a)
+console.log(b)
+console.log(c)
 
-// // if (true){
-// //     // let a = 20
-// //     // const b = 30
-// //     var c = 40      // var will always work inside 
-// // }
+if (true){
+    // let a = 20
+    // const b = 30
+    var c = 40      // var will always work inside 
+}
 
-// // // console.log(a)
-// // // console.log(b)
-// // console.log(c)
-
-
-// //Local variables cannot be accessed outside the function
-
-// let b = 100
-// if (true){
-   
-//     const b = 30
-//     console.log(b);
-    
-// }
-
-// // console.log(a)
+// console.log(a)
 // console.log(b)
-// // console.log(c)
+console.log(c)
 
-// //global scope is different in browser and when we run it in node
+
+//Local variables cannot be accessed outside the function
+
+let f = 100
+if (true){
+   
+    const f = 30
+    console.log(f);
+    
+}
+
+// console.log(a)
+console.log(f)
+// console.log(c)
+
+//global scope is different in browser and when we run it in node
 
 
 
