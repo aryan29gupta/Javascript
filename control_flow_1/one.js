8bc4a18c7c73d4c97b627aae7a1c2b7487d4e4f1
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-17 18:00:10 +0000

    If Else Elif

---
+++ control_flow_1/one.js
@@ -0,0 +1,32 @@
+// // =, <, >, <=, >=,==, === 
+// // === checks datatype as well as value
+// // == checks only value and ignores datatype
+
+// if (2 == "2"){
+//     console.log("executed");
+// }
+
+// if (2 === "2"){
+//     console.log("executed");
+// }
+// else{
+//     console.log("not equal");
+    
+// }
+
+const a = "hi"
+
+if (a=="hi"){
+    console.log(a);
+    var b = "Aryan"  //therefore always use const or let because due to var the value of variable b will be accessed outside which is not correct.
+    console.log(b);
+}
+
+console.log(b);
+
+// if (a=="hi") console.log("This is implicit scope"),
+// console.log("2");
+
+
+
+
