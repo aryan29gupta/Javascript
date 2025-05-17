6d0784b940371d150093065361cace1837114c38
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-17 18:16:41 +0000

    and, or

---
+++ control_flow_1/two.js
@@ -0,0 +1,11 @@
+const phone = true
+const email = true
+const area = false
+if (phone && email){
+    console.log("allow to enter");
+}
+
+if (phone || area){
+    console.log("Allow to Enter");
+    
+} 
