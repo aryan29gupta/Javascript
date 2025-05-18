f25915ba1d144e95c1bea8a7911a6734d8ba361f
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-18 20:05:50 +0000

    For Each

---
+++ loops/forEach.js
@@ -0,0 +1,38 @@
+// const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach(function (val){
+//     console.log(val);
+// })
+
+// coding.forEach((val) => {
+//     console.log(val);
+    
+// })
+
+// function printvalue(value){
+//     console.log(value);
+// }
+
+// coding.forEach(printvalue) //give reference of function and not execute by printvalue()
+
+// coding.forEach((item,index,arr) => {
+//     console.log(item,index,arr);
+// })
+
+
+objarray = [
+    {
+       language:"hindi",
+    },
+    {
+       language:"english",
+    },
+    {
+       language:"spanish",
+    }
+]
+
+objarray.forEach((item) => {
+    console.log(item.language);
+    
+});
