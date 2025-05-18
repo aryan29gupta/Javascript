70cddbae2f5f30af5094baba20146d9b27dee471
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-18 14:25:00 +0000

    While and Do While

---
+++ loops/while_dowhile.js
@@ -0,0 +1,20 @@
+// let index = 1
+// while (index <= 2) {
+//     console.log("hi");
+//     index++
+// }
+
+let arr = 0
+const myarr = ["Aryan","Rajiv","Nikhil"]
+while (arr<myarr.length) {
+    console.log(myarr[arr])
+    arr++
+}
+
+
+let score = 100
+do{
+    console.log(score);
+    score++
+}
+while(score <=105);
