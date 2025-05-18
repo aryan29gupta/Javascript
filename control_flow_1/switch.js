f05d313c1fb2ca95da1556e24860aa7701a6346c
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-18 12:11:10 +0000

    Switch condition

---
+++ control_flow_1/switch.js
@@ -0,0 +1,19 @@
+const num = 5
+
+switch (num) {
+    case 1://string or any other datatype can also be written in place of 1
+        console.log("Monday");    
+        break;
+    case 2:
+        console.log("Tuesday");    
+        break;
+    case 3:
+        console.log("Wednesday");    
+        break;
+    case 4:
+        console.log("Thursday");    
+        break;
+    default:
+        console.log("Enter valid data");
+        break;
+}
