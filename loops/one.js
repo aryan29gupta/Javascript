b2fb49788cf7862e859876f090d52c281d3c0123
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-18 13:39:24 +0000

    Loops

---
+++ loops/one.js
@@ -0,0 +1,17 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+for (let i = 0; i <= 5; i++) {
+    for (let j = 0; j <= 5; j++) {
+        console.log("*");
+    } 
+}
+
+//break and continue
+//break: iterates the loop, no value will be print afterwards
+//continue: will continue the loop and the current value will be skipped but rest all will remain same.
+
