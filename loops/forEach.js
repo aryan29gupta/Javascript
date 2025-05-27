9b5b642681c1154003414e47a378269c48f8c61b
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2025-05-27 18:44:54 +0000

    Removed comments

--- loops/forEach.js
+++ loops/forEach.js
@@ -1,23 +1,23 @@
-// const coding = ["js","ruby","java","python","cpp"]
+const coding = ["js","ruby","java","python","cpp"]
 
-// coding.forEach(function (val){
-//     console.log(val);
-// })
+coding.forEach(function (val){
+    console.log(val);
+})
 
-// coding.forEach((val) => {
-//     console.log(val);
-    
-// })
+coding.forEach((val) => {
+    console.log(val);
 
-// function printvalue(value){
-//     console.log(value);
-// }
+})
 
-// coding.forEach(printvalue) //give reference of function and not execute by printvalue()
+function printvalue(value){
+    console.log(value);
+}
 
-// coding.forEach((item,index,arr) => {
-//     console.log(item,index,arr);
-// })
+coding.forEach(printvalue) //give reference of function and not execute by printvalue()
+
+coding.forEach((item,index,arr) => {
+    console.log(item,index,arr);
+})
 
 
 objarray = [
